4efc4de5df2f337182a47d4072d14002b2987c52
Vikas Sangwan <Vikassangwan2256@gmail.com>
2026-03-12 21:21:56 -0700

    Update task.py

--- task.py
+++ task.py
@@ -1,10 +1,10 @@
-# print((1300/5)*1.12)
-# print("Welcome to the tip calculator!")
-# bill = float(input("What was the total bill? $"))
-# tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-# people = int(input("How many people to split the bill? "))
+# Tip Calculator
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
 
-# print("Each person should pay: " + str(round((bill / people) * (1+tip/100), 2)))
+print("Each person should pay: " + str(round((bill / people) * (1+tip/100), 2)))
 
 # Tip Calculator
 print("Welcome to the tip calculator")
